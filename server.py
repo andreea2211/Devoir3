9c3672dc7ed50c95f1cf3ed091b62dc1c9eaaf3e
andreea2211 <andreea.dan06@yahoo.com>
2017-04-26 10:34:18 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
-Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
+
 >>> import sys
 import socket
 import select
@@ -22,7 +21,7 @@ def chat_server():
 
     serversocket.bind((host, port))
 
-    # la file d'attente jusqu'à cinq demandes
+    # la file d'attente jusqu'Ã  cinq demandes
 
     serversocket.listen(5)
 
